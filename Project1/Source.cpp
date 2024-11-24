4fd7c4ed6fa217f58690725bf809d5593b801bda
Fahad Bin Faisal <fahadfaisalnawaz1000@gmail.com>
2024-11-24 16:54:40 +0500

    Added insertion(FINALLY) You can also send path of root node into the AVL's constructor and it will generate its tree according to the root file

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
+#include "AVL.h"
 using namespace std;
 
-int main() {
-	
-	cout << "Hello" << endl;
+int main()
+{
+	AVL<int> tree;
+	tree.insert(1);
+	tree.insert(2);
+	tree.insert(3);
+	tree.insert(4);
+	tree.insert(5);
+	tree.insert(6);
+	tree.insert(7);
+	tree.insert(7);
+	tree.insert(4);
+	tree.saveRootToFile();
+
+
+	ifstream root("AVL_Root.txt");
+	if (root.is_open())
+	{
+		string rootPath;
+		root >> rootPath;
+		root.close();
+
+		AVL<int> tree2(rootPath);
+		tree2.print();
+	}
 
 	return 0;
 }
