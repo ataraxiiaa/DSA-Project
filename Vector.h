eecb87415eb318fb1de6b7d10516eedd805e0cec
Fahad Bin Faisal <fahadfaisalnawaz1000@gmail.com>
2024-12-02 21:23:41 +0500

    Optimized vector

--- Vector.h
+++ Vector.h
@@ -17,34 +17,37 @@ public:
 		currSize = 0;
 		maxSize = 5;
 	}
-	//~Vector() { delete ptr; } // Destructor
+	~Vector() { delete[] ptr; } // Destructor
 
 	// Adding values to the array
 	void push_back(Type value) {
 		if (currSize == maxSize) {
-			Type* temp = new Type[maxSize];
+			Type* temp = new Type[2 * maxSize];
 			for (int i = 0; i < maxSize; ++i)
 				temp[i] = ptr[i];
 			delete[] ptr;
-			ptr = new Type[2 * maxSize];
-			for (int i = 0; i < maxSize; ++i)
-				ptr[i] = temp[i];
-			delete[] temp;
-			maxSize *= 2;
+			maxSize<<1;
+			ptr = temp;
 		}
 		ptr[currSize] = value;
 		++currSize;
 	}
 	int getCurr() { return currSize; }
 
-	Type& operator[](int index) { return ptr[index]; } // Accessing address the pointer is pointing to 
+	Type& operator[](int index) 
+	{ 
+		if (index < currSize && index>=0)
+			return ptr[index];
+		else
+			throw runtime_error("Accessing invalid index.");
+	} // Accessing address the pointer is pointing to 
 
 	void Destroy(int index) {
 		if (index == currSize - 1) {
 			currSize--;
 		}
 		else {
-			for (int i = index; i < currSize - 1; ++i) { // Discarding the vakue not required
+			for (int i = index; i < currSize - 1; ++i) { // Discarding the value not required
 				ptr[i] = ptr[i + 1]; // Move the values by 1
 			}
 			currSize--; // Adjust the current Size
@@ -54,62 +57,5 @@ public:
 		return ptr[currSize - 1];
 	}
 };
-
-template <typename Type> class Vector<Type*>  // Template of vector created for pointer to pointer
-{
-	Type** ptr; // pointer to pointer to type
-	int currSize, maxSize; // Used to track if to expand the array or ot
-public:
-	Vector() // Default constructor 
-	{
-		ptr = new Type * [5];
-		currSize = 0;
-		maxSize = 5;
-	}
-	/*~Vector() {
-		for (int i = 0; i < currSize; ++i) {
-			if (ptr[i] != nullptr)
-				delete[] ptr[i];
-		}
-			delete[] ptr;
-	}*/
-	int GetSize()const { return this->currSize; }
-	void push_back(Type* value) {
-		if (currSize == maxSize) {
-			Type** temp = new Type * [maxSize];
-			for (int i = 0; i < maxSize; ++i)
-				temp[i] = ptr[i];
-			delete[] ptr;
-			ptr = new Type * [2 * maxSize];
-			for (int i = 0; i < maxSize; ++i)
-				ptr[i] = temp[i];
-			delete[] temp;
-			maxSize *= 2;
-		}
-		ptr[currSize] = value;
-		++currSize;
-	}
-	Type*& operator[](int index)
-	{
-		return ptr[index];
-	}
-	void Destroy(int index) {
-		if (index == currSize - 1) {
-			--currSize;
-		}
-		else {
-			for (int i = index; i < currSize - 1; ++i) { // Discarding the vakue not required
-				ptr[i] = ptr[i + 1]; // Move the values by 1
-			}
-			--currSize; // Adjust the current Size
-		}
-	}
-	Type*& back() {
-		return ptr[currSize - 1];
-	}
-
-
-};
-
 
 #endif // Vector_H
