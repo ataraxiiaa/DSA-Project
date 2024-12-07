3ef429ae5f6594f298f3a6c3e0d3c0694d845878
Ali Abdullah <157208557+ataraxiiaa@users.noreply.github.com>
2024-12-08 02:50:13 +0500

    .

--- Source.cpp
+++ Source.cpp
@@ -21,84 +21,84 @@ int main()
 	//tree1.remove(70);
 	//tree1.remove(80);
 	//tree1.printTree(tree1.getRoot());
-	//GitLite git;
-	//String input;
-	//while (true)
-	//{
-	//	cout << "> ";
-	//	input = "";
-	//	cin >> input;
+	GitLite git;
+	String input;
+	while (true)
+	{
+		cout << "> ";
+		input = "";
+		cin >> input;
 
-	//	if (input == "init")
-	//	{
-	//		git.initRepo();
-	//	}
-	//	else if (input == "print")
-	//	{
-	//		git.printCurrentTree();
-	//	}
-	//	else if (input == "current-branch")
-	//	{
-	//		git.printCurrentBranch();
-	//	}
-	//	else if (input == "branches")
-	//	{
-	//		git.printAllBranches();
-	//	}
-	//	else if (input == "create-branch")
-	//	{
-	//		git.createBranch();
-	//	}
-	//	else if (input == "checkout")
-	//	{
-	//		git.checkout();
-	//	}
-	//	else if (input == "delete-branch")
-	//	{
-	//		git.deleteBranch();
-	//	}
-	//	else if (input == "save")
-	//	{
-	//		git.saveRepoToFile();
-	//		cout << "Repository data saved to file: "<< git.getRepoDataFile() << endl;
-	//	}
-	//	else if (input == "load")
-	//	{
-	//		filesystem::path temp;
-	//		cout << "Enter REPO_DATA.txt file path: ";
-	//		cin >> temp;
-	//		if (filesystem::exists(temp))
-	//		{
-	//			git.loadRepoFromFile(temp);
-	//			cout << "Repository loaded from: " << git.getRepoDataFile() << endl;
-	//		}
-	//		else
-	//			cout << "Error: Invalid file path." << endl;
-	//	}
-	//	else if (input == "insert")
-	//	{
-	//		git.insert();
-	//	}
-	//	else if (input == "search")
-	//	{
-	//		git.search();
-	//	}
-	//	else if (input == "remove")
-	//	{
-	//		git.remove();
-	//	}
-	//	else if (input == "update")
-	//	{
-	//		git.update();
-	//	}
-	//	else if (input == "exit")
-	//	{
-	//		break;
-	//	}
-	//	else
-	//	{
-	//		cout << "Invalid command.\n";
-	//	}
-	//}
+		if (input == "init")
+		{
+			git.initRepo();
+		}
+		else if (input == "print")
+		{
+			git.printCurrentTree();
+		}
+		else if (input == "current-branch")
+		{
+			git.printCurrentBranch();
+		}
+		else if (input == "branches")
+		{
+			git.printAllBranches();
+		}
+		else if (input == "create-branch")
+		{
+			git.createBranch();
+		}
+		else if (input == "checkout")
+		{
+			git.checkout();
+		}
+		else if (input == "delete-branch")
+		{
+			git.deleteBranch();
+		}
+		else if (input == "save")
+		{
+			git.saveRepoToFile();
+			cout << "Repository data saved to file: "<< git.getRepoDataFile() << endl;
+		}
+		else if (input == "load")
+		{
+			filesystem::path temp;
+			cout << "Enter REPO_DATA.txt file path: ";
+			cin >> temp;
+			if (filesystem::exists(temp))
+			{
+				git.loadRepoFromFile(temp);
+				cout << "Repository loaded from: " << git.getRepoDataFile() << endl;
+			}
+			else
+				cout << "Error: Invalid file path." << endl;
+		}
+		else if (input == "insert")
+		{
+			git.insert();
+		}
+		else if (input == "search")
+		{
+			git.search();
+		}
+		else if (input == "remove")
+		{
+			git.remove();
+		}
+		else if (input == "update")
+		{
+			git.update();
+		}
+		else if (input == "exit")
+		{
+			break;
+		}
+		else
+		{
+			cout << "Invalid command.\n";
+		}
+	}
 	return 0;
 }
